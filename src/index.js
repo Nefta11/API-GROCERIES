433c20f880c2ef5ce86523d270635fdeefdef0ec
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-12 18:54:21 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const express=require('express');
 const app=express();
 
 app.get('/home',(req,res)=>{
-
+    //Aqui se procesa la peticion y se envia una respuesta
+    res.json({
+        "status":"Petición por get recibida..."
+    }
+    )
 });
 
 app.listen(3200,()=>{
