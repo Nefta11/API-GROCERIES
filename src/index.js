3246e04b7ad3a47dd5254a615777006e7d950a27
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-12 19:04:41 -0600

    update

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@ app.get('/home',(req,res)=>{
     //Aqui se procesa la peticion y se envia una respuesta
     res.json({
         "status":"Petición por get recibida..."
-    }
-    )
+    });
 });
 
-app.listen(3200,()=>{
+app.post('/home', (req, res) => {
+    // Aquí se procesa la petición POST y se envía una respuesta
+    res.json({
+        "status": "Petición por POST recibida..."
+    });
+});
+
+app.delete('/home', (req, res) => {
+    // Aquí se procesa la petición DELETE y se envía una respuesta
+    res.json({
+        "status": "Petición por DELETE recibida..."
+    });
+});
+
+
+app.listen(3000,()=>{
     console.log("server on port 3200");
 });
