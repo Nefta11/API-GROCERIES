b412af1fcbb2b9143d9407f5871528e4a8f2e9dc
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-15 17:20:02 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-
+app.use(express.json)
 app.get('/getAll',(req,res)=>{
     //Aqui se procesa la peticion y se envia una respuesta
     res.json({
@@ -8,14 +8,47 @@ app.get('/getAll',(req,res)=>{
     });
 });
 
-app.get("/getOne/:barcode",(req,res)=>{
-    console.log(req.params.barcode);
+/*app.get("/getOne/:barcode",(req,res)=>{
+    console.log(req.params);
     res.json({
         "status":"Petición por get recibida siuu..."
     }); 
     
+});*/
+
+app.get("/getOne/:numero", (req, res) => {
+    const numero = req.params.numero;
+    const numEntero = parseInt(numero);
+
+    let binario = "";
+    let n = numEntero;
+    
+    do {
+        binario = (n % 2) + binario;
+        n = Math.floor(n / 2);
+    } while (n > 0);
+
+    res.json({
+        status: "Petición por get recibida siuu...",
+        binario: binario
+    });
 });
+
+
+
+app.post("/insertProduct",(req,res)=>{
+console.log(req.body)
+res.json({
+    status:"producto insertado"
+})
+
+
+})
+
 
 app.listen(3000,()=>{
     console.log("server on port 3200");
 });
+
+//soy dos siuuu 
+//"result":0101
