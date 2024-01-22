0470ea3eacf9dbba5a5dce55c7faf16dcd36ba3b
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-22 10:13:46 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express= require('express');
-const app=express();
+const express = require('express');
+const app = express();
 app.use(express.json())
 
 
@@ -49,54 +49,56 @@ res.json({
 
 
 
-baseDeDatos =[{
-    "id":0,
-    "nombre":"Neftali",
-    "apellido":"Vergara"
+baseDeDatos = [{
+    "id": 0,
+    "nombre": "Neftali",
+    "apellido": "Vergara"
 },
-{"id":1,
-"nombre":"Avryl",
-"apellido":"Salas"}]
+{
+    "id": 1,
+    "nombre": "Avryl",
+    "apellido": "Salas"
+}]
 
-app.post("/insertOne",(req,res)=>{
-    const newDate= req.body;
+app.post("/insertOne", (req, res) => {
+    const newDate = req.body;
     baseDeDatos.push(newDate)
-    res.json({status:"Usuario Insertado Correctamente"})
+    res.json({ status: "Usuario Insertado Correctamente" })
 });
 
 
-app.get("/getAll",(req,res)=>{
+app.get("/getAll", (req, res) => {
     res.json(baseDeDatos)
-    });
+});
 
 
 app.get("/getOne/:id", (req, res) => {
     // Obtener el ID de la solicitud
     const idBuscado = parseInt(req.params.id);
     const resultado = baseDeDatos.find(usuario => usuario.id === idBuscado);
-  
+
     if (resultado) {
-      res.json(resultado);
+        res.json(resultado);
     } else {
-      res.json({ mensaje: "Usuario no encontrado" });
+        res.json({ mensaje: "Usuario no encontrado" });
     }
-  });
+});
 
 
-app.delete("/deleteOne/id",(req,res)=>{
+app.delete("/deleteOne/id", (req, res) => {
 
 });
 
-app.put("/updateOne/id",(req,res)=>{
+app.put("/updateOne/id", (req, res) => {
 
 })
 
 
 
 
-app.listen(3200,()=>{
+app.listen(3200, () => {
     console.log("server on port 3200 Siuuu");
 });
 
-//soy dos siuuu 
+//soy dos siuuu
 //"result":0101
